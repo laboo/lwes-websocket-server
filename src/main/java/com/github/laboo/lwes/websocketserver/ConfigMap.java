e365adbc87a5c8975c582caacb291d1c70ce0073
Mark Libucha <mlibucha@gmail.com>
2017-06-03 14:32:19 -0700

    Fixed formatting, typos, and removeClientConfig logic.

--- src/main/java/com/github/laboo/lwes/websocketserver/ConfigMap.java
+++ src/main/java/com/github/laboo/lwes/websocketserver/ConfigMap.java
@@ -47,51 +47,52 @@ public enum ConfigMap {
     }
 
     public static int numConfigs(String key) {
-	PSet<ClientConfig> set = requestMap.get(key);
+        PSet<ClientConfig> set = requestMap.get(key);
         return (set != null) ? set.size() : 0;
     }
 
     public static synchronized ClientConfig addClientConfig(ClientConfig clientConfig) {
-	ClientConfig out = clientConfig;
-	for (String name: clientConfig.getRequests().keySet()) {
+        ClientConfig out = clientConfig;
+        for (String name: clientConfig.getRequests().keySet()) {
             String requestKey = clientConfig.getChannel() + name;
-	    PSet<ClientConfig> existing = requestMap.get(requestKey);
+            PSet<ClientConfig> existing = requestMap.get(requestKey);
             if (existing == null) {
                 PSet<ClientConfig> newSet = HashTreePSet.empty();
                 newSet = newSet.plus(clientConfig);
                 requestMap = requestMap.plus(requestKey, newSet);
             } else {
-		for (ClientConfig c : existing) {
-		    if (c.equals(clientConfig)) {
-			// If we've already got an "equals" client config, we don't want to replace it
-			// with the new one. We want to leave the old one in place, and later the connection
-			// for the new client config will get added to the old client config.
-			out = c;
-			continue;
-		    } else {
-			existing = existing.plus(clientConfig);
-			requestMap = requestMap.plus(requestKey, existing);
-		    }
-		}
-	    }
+                for (ClientConfig c : existing) {
+                    if (c.equals(clientConfig)) {
+                        // If we've already got an "equals" client config, we don't want to replace it
+                        // with the new one. We want to leave the old one in place, and later the connection
+                        // for the new client config will get added to the old client config.
+                        out = c;
+                        continue;
+                    } else {
+                        existing = existing.plus(clientConfig);
+                        requestMap = requestMap.plus(requestKey, existing);
+                    }
+                }
+            }
         }
-	return out;
+        return out;
     }
 
     public synchronized static void removeClientConfig(ClientConfig clientConfig) {
         if (clientConfig.clients.size() != 0) {
             return;
         }
-        Map<String,Set<ClientConfig>> newRequestMap = new HashMap<>();
         for (Map.Entry<String,PSet<ClientConfig>> entry : requestMap.entrySet()) {
-	    PSet<ClientConfig> set = entry.getValue();
+            PSet<ClientConfig> set = entry.getValue();
             for (ClientConfig cc : set) {
                 if (cc.equals(clientConfig)) {
-		    set = set.minus(cc);
+                    set = set.minus(cc);
                 }
             }
             if (set.isEmpty()) {
-		requestMap = requestMap.minus(entry.getKey());
+                requestMap = requestMap.minus(entry.getKey());
+            } else {
+                entry.setValue(set);
             }
         }
     }
